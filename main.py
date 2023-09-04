04cba4f99233a35f39fc2f87b79f41c326e20aca
yojulab <otter.oh@gmail.com>
2023-09-04 13:25:05 +0900

    add somethings

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 # conda install -c conda-forge fastapi uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# No 'Access-Control-Allow-Origin'
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 실제 운영 환경에서는 접근 가능한 도메인만 허용하는 것이 좋습니다.
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
